82f580e97832222b6e9fef7e02fa6a6f319212cd
Ryoichi Izumita <r.izumita@caph.jp>
2015-09-28 22:53:25 +0900

    Initial Commit

---
+++ ResourceInstantiatable/ResourceInstantiatable.h
@@ -0,0 +1,19 @@
+//
+//  ResourceInstantiatable.h
+//  ResourceInstantiatable
+//
+//  Created by 和泉田 領一 on 2015/09/28.
+//  Copyright © 2015年 CAPH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ResourceInstantiatable.
+FOUNDATION_EXPORT double ResourceInstantiatableVersionNumber;
+
+//! Project version string for ResourceInstantiatable.
+FOUNDATION_EXPORT const unsigned char ResourceInstantiatableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResourceInstantiatable/PublicHeader.h>
+
+
